a6a6184fda8b6a1c03145d40baef1a56dc42bf33
hkj23 <75363520+hkj23@users.noreply.github.com>
2021-12-08 22:39:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var rbutton = document.getElementById("random");
+
+function generateRandomColor() {
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+	  color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+  }
+  
+  
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+rbutton.addEventListener("click",function(){
+	color1.value=generateRandomColor();//"#F10531"
+	color2.value=generateRandomColor();//"#F10531"
+	setGradient();
+
+})
